683e64e71ccde497d76d66f65c19a252503a1884
Entity Black <entityblack@gmail.com>
2016-11-29 11:42:47 +0100

    basic structure

--- src/xsd-mapping.js
+++ src/xsd-mapping.js
@@ -1,1 +1,28 @@
-module.exports = {};
+var rules = require("./rules");
+
+
+/**
+ * 
+ * @param xsd must have format of parsed scheme
+ * 
+ */
+var map = function (data, xsd) {
+
+};
+
+var Mapping = function (xsd) {
+    this.xsd = xsd;
+};
+
+Mapping.prototype = {
+    map: function (data) {
+        return map(data, this.xsd);
+    }
+};
+
+module.exports = {
+    map: map,
+    mapping: function (xsd) {
+        return new Mapping(xsd);
+    }
+};
